1843abcb6635b0ac27c91bbda4acaff02d83076c
aaryatallada <125602822+aaryatallada@users.noreply.github.com>
2023-12-02 22:08:42 -0800

    write block bitmap

--- ext2-create.c
+++ ext2-create.c
@@ -284,6 +284,12 @@ void write_block_bitmap(int fd)
 
 	// TODO It's all yours
 	u8 map_value[BLOCK_SIZE];
+	for(int i = 0; i < BLOCK_SIZE; i++){
+		if(i < 24)
+			map_value[i] = 1;
+		else
+			map_value[i] = 0;
+	}
 
 	if (write(fd, map_value, BLOCK_SIZE) != BLOCK_SIZE)
 	{
