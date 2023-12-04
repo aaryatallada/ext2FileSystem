2bf0c18772d46d718803b1ffcb4c70ba6647bf67
aaryatallada <125602822+aaryatallada@users.noreply.github.com>
2023-12-04 01:52:12 -0800

    Update ext2-create.c

--- ext2-create.c
+++ ext2-create.c
@@ -526,7 +526,7 @@ void write_hello_world_file_block(int fd)
 		errno_exit("lseek");
 	}
 	ssize_t bytes_remaining = BLOCK_SIZE;
-	const char *myString = "Hello world\n";
+	const char *myString = "Hello world\0x00";
 	write(fd, myString, 11);
 }
 
