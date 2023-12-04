ee3e99561f3555a02eac8f7cddaa2f2f1ef5802a
aaryatallada <125602822+aaryatallada@users.noreply.github.com>
2023-12-04 01:46:19 -0800

    Update ext2-create.c

--- ext2-create.c
+++ ext2-create.c
@@ -414,7 +414,7 @@ void write_inode_table(int fd) {
 	hello_sym_inode.i_gid = 1000;
 	hello_sym_inode.i_links_count = 1;
 	hello_sym_inode.i_blocks = 0; //SYMLINKS JUST REFERENCE TO THE SAME BLOCK
-	strcpy(hello_inode.i_block, "hello-world");
+	strcpy(hello_sym_inode.i_block, "hello-world");
 	//hello_world_inode.i_block[0] = HELLO_WORLD_FILE_BLOCKNO;
 	write_inode(fd, HELLO_INO, &hello_sym_inode);
 
